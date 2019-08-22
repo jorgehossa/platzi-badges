2fbe5298f01a30660d55a9719d8773df2ebc9b32
Jorge Ossa <jorgehossa@gmail.com>
2019-08-22 08:28:18 -0500

    clean index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const jsx = <h1>Hello, Platzi Badges!</h1>;
-// const element = React.createElement(
-//   'a',
-//   { href: 'https://platzi.com' },
-//   'Ir a Platzi'
-// );
-// const name = 'Stephanie';
-// const sum = () => 3 + 3;
-// const element = React.createElement('h1', {}, `Hola, soy ${name}`);
-// const jsx = <h1>Hola soy, {undefined}</h1>;
+const element = <div>
+    <h1>Hola Mundo</h1>
+</div>
 
-const jsx = (
-  <div>
-    <h1>Hola, soy Richard</h1>
-    <p>Soy ingeniero frontend.</p>
-  </div>
-);
-
-const element = React.createElement(
-  'div',
-  {},
-  React.createElement('h1', {}, 'Hola, soy Richard'),
-  React.createElement('p', {}, 'Soy ingeniero de la web.')
-);
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(element, container);
