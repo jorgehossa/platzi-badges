2d391b86e1f390dd5061387fde9b4b03a2d463c3
Jorge Ossa <jorgehossa@gmail.com>
2019-08-23 23:04:37 -0500

    Setting my component prop's

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -11,12 +11,12 @@ class Badge extends React.Component {
         <img src={confLogo} alt="Logo conferencia"/>
       </header>
       <section className='Badge__section-name'>
-        <img className='Badge__avatar' src="https://secure.gravatar.com/avatar/d8cc91554f3b52d1d54a08d299e30e33" alt="Avatar"/>
-        <h1>Jorge <br /> Ossa</h1>
+        <img className='Badge__avatar' src={this.props.avatarUrl} alt="Avatar"/>
+        <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
       </section>
       <section className="Badge__section-info">
-        <h3>Frontend Developer</h3>
-        <div>@jorgehossa</div>
+        <h3>{this.props.jobTitle}</h3>
+        <div>@{this.props.twitter}</div>
       </section>
       <footer className='Badge__footer'>
         #platziConf
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(<Badge firstName='Jorge' lastName='Ossa' avatarUrl='https://secure.gravatar.com/avatar/d8cc91554f3b52d1d54a08d299e30e33' jobTitle='Frontend Developer' twitter='jorgehossa'/>, container);
